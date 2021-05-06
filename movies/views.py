a7c5294bcdaaf130c1b0ef40953c2c67a8f47dcb
siewla <siewlapang@gmail.com>
2021-05-06 20:46:37 +0800

    add permission to movies view

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,13 @@
 from movies.serializers import MovieSerializer
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from movies.models import *
 from movies.serializers import *
 
 # Create your views here.
+@permission_classes([IsAuthenticated])
 @api_view(['GET', 'POST'])
 def index_view(request):
     if request.method == 'GET':
