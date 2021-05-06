eda143a3f09e7993dbd3cb5ee9f4335d2035d1eb
siewla <siewlapang@gmail.com>
2021-05-06 19:03:05 +0800

    setup view with serializer

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,13 @@
+from movies.serializers import MovieSerializer
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from movies.models import *
+from movies.serializers import *
 
 # Create your views here.
 @api_view(['GET'])
 def index_view(request):
-    return Response({'message': 'it works'})
+    movies = Movie.objects.all()
+    serializer= MovieSerializer(movies, many=True)
+    return Response(serializer.data)
