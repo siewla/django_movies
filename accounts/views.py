b0ecfc3035ad926d1bc124bd36d8042233789070
siewla <siewlapang@gmail.com>
2021-05-06 19:49:24 +0800

    register route

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,16 @@
+from accounts.serializers import UserSerializer
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from accounts.serializers import *
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 def register_view(request):
-    pass
+    if request.method == 'POST':
+        user = UserSerializer(data=request.data)
+        if user.is_valid():
+            user.save()
+            return Response({"message": "user is registered"})
+        else:
+            return Response({"message": "user is not registered"})
